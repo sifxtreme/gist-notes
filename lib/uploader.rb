b8e972de5a4aed9a178243b934e46b4ba9b7101b
Asif Ahmed <asif.h.ahmed@gmail.com>
2020-04-16 16:37:33 -0700

    Fix Bug Where Deleting File will Delete the Gist, Lint

--- lib/uploader.rb
+++ lib/uploader.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative './config'
 
 require 'octokit'
@@ -5,8 +7,7 @@ require 'pry'
 
 module GistNotes
   class Uploader
-
-    INDEX_GIST_NAME="notes.md"
+    INDEX_GIST_NAME = 'notes.md'
 
     def first_time_upload
       sync_folders_up
@@ -16,39 +17,41 @@ module GistNotes
     def folder_paths_to_sync
       folders = Dir.glob "#{Config::GIST_NOTES_PATH}/*/"
 
-      folders.select { |x| is_folder_syncable?(x) }
+      folders.select { |x| folder_syncable?(x) }
     end
 
-    def is_folder_syncable?(folder)
+    def folder_syncable?(folder)
       files = files_in_folder(folder)
 
       # dont sync if folder has no valid files
       return false if files.nil? || files.empty?
-      
+
       true
     end
 
     def files_in_folder(folder)
-      files = Dir.glob("#{folder}*").select{ |e| File.file? e }
-      file_hash = files.map { |x| [x,File.read(x)] }.to_h
+      files = Dir.glob("#{folder}*").select { |e| File.file? e }
+      file_hash = files.map { |x| [x, File.read(x)] }.to_h
 
       # dont sync blank files
-      file_hash.reject! { |_,x| x.nil? || x.empty? }
+      file_hash.reject! { |_, x| x.nil? || x.empty? }
 
       file_hash
     end
 
     def sync_root_index_file_up
       refresh_gists
-            
-      gists_on_device = folder_paths_to_sync.map {|x| get_folder_name(x)}
 
-      gists_on_device_and_online = gists.select {|x| gists_on_device.include?(x[:description]) }
+      gists_on_device = folder_paths_to_sync.map { |x| get_folder_name(x) }
+
+      gists_on_device_and_online = gists.select do |x|
+        gists_on_device.include?(x[:description])
+      end
 
       index_file = create_index_file(gists_on_device_and_online)
 
-      index_file_hash_to_upload = { 
-        INDEX_GIST_NAME => {content: index_file}
+      index_file_hash_to_upload = {
+        INDEX_GIST_NAME => { content: index_file }
       }
 
       upload_gist(INDEX_GIST_NAME, index_file_hash_to_upload)
@@ -57,30 +60,29 @@ module GistNotes
     def add_folder(path)
       puts "add_folder #{path}"
 
-      folder_path = path.split("/")[0..-2].join("/")
+      folder_path = path.split('/')[0..-2].join('/')
       sync_folder_up("#{folder_path}/")
-      
+
       sync_root_index_file_up
     end
 
     def delete_folder(path)
       refresh_gists
-      folder_name, _ = path.gsub(Config::GIST_NOTES_PATH, "").split("/")
+      folder_name, = path.gsub(Config::GIST_NOTES_PATH, '').split('/')
 
-      gist_to_delete = gists.select {|x| x[:description] == folder_name}.first
+      gist_to_delete = gists.select { |x| x[:description] == folder_name }.first
       client.delete_gist(gist_to_delete[:id]) if gist_to_delete
-      
+
       sync_root_index_file_up
     end
 
     def get_folder_name(path)
-      path.gsub(Config::GIST_NOTES_PATH, "").split("/").reject {|x| x.empty?}.first
+      path.gsub(Config::GIST_NOTES_PATH, '').split('/').reject(&:empty?).first
     end
 
     def format_file_hash_for_upload(files)
-      files.map { |file_path, content| [file_path.split("/").last, {content: content}] }.to_h
+      files.map { |file_path, content| [file_path.split('/').last, { content: content }] }.to_h
     end
-
 
     def sync_folders_up
       folder_paths_to_sync.each do |folder_path|
@@ -99,7 +101,7 @@ module GistNotes
 
       # insert blank file at the beginning with names of other files
       index_file_hash = {
-        "_#{folder_name}.md" => {content: create_index_file_for_gist(files_to_sync.keys)}
+        "_#{folder_name}.md" => { content: create_index_file_for_gist(files_to_sync.keys) }
       }
       files_to_sync = index_file_hash.merge(files_to_sync)
 
@@ -109,24 +111,25 @@ module GistNotes
     def upload_gist(folder_name, files_to_sync)
       puts "upload_gist #{folder_name}"
 
-      if existing_gist = get_gist_for(folder_name)
-        files_on_github = existing_gist[:files].map {|k,_| k.to_s}
+      if (existing_gist = get_gist_for(folder_name))
+
+        files_on_github = existing_gist[:files].map { |k, _| k.to_s }
         files_to_delete = files_on_github - files_to_sync.keys
 
         new_files_to_sync = files_to_sync.merge!(files_to_delete.map { |x| [x, nil] }.to_h)
 
-        client.edit_gist(existing_gist[:id], {files: new_files_to_sync})
+        client.edit_gist(existing_gist[:id], files: new_files_to_sync)
       else
         client.create_gist(
-          description: folder_name, 
-          public: false, 
+          description: folder_name,
+          public: false,
           files: files_to_sync
         )
       end
     end
 
     def client
-      @client ||= Octokit::Client.new(access_token: ENV["GIST_GITHUB_TOKEN"])
+      @client ||= Octokit::Client.new(access_token: ENV['GIST_GITHUB_TOKEN'])
     end
 
     def user
@@ -142,34 +145,42 @@ module GistNotes
     end
 
     def get_gist_for(description)
-      gists.select {|x| x[:description] == description}.first
+      gists.select { |x| x[:description] == description }.first
     end
 
     # consider moving to different class
     def create_index_file_for_gist(filepath_array)
-      filepath_array.map {|x| "- #{x.split('/').last}"}.join("\n")
+      filepath_array
+        .map { |x| "- #{x.split('/').last}" }
+        .sort
+        .join("\n")
     end
 
     def create_index_file(my_gists)
-      ["# NOTES", "", "", my_gists.map {|x| md_for_gist(x)}].flatten.join("\n")
+      ['# NOTES', '', '', my_gists
+        .map { |x| md_for_gist(x) }]
+        .flatten
+        .sort
+        .join("\n")
     end
 
     def md_for_gist(gist)
       [
         "### [#{gist[:description]}](#{gist[:html_url]})",
-        "",
+        '',
         md_for_gist_files(gist).compact,
-        "",
-        "",
+        '',
+        ''
       ].flatten.join("\n")
     end
 
     def md_for_gist_files(gist)
-      gist[:files].map do |_,file|
-        next if "_#{gist[:description]}.md" == file[:filename] # we created a special index file in a gist
+      gist[:files].map do |_, file|
+        # we created a special index file in a gist
+        next if file[:filename] == "_#{gist[:description]}.md"
+
         "- [#{file[:filename]}](#{gist[:html_url]}#file-#{file[:filename].gsub('.', '-').gsub(' ', '-')})"
       end
     end
-
   end
 end
--- lib/watcher.rb
+++ lib/watcher.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative './config'
 require_relative './uploader'
 
@@ -8,32 +10,35 @@ def gist_notes
   GistNotes::Uploader.new
 end
 
-def is_valid_file_path?(path)
-  [1,2].include?(path.gsub(Config::GIST_NOTES_PATH, "").split("/").count)
+def valid_file_path?(path)
+  [1, 2].include?(path.gsub(Config::GIST_NOTES_PATH, '').split('/').count)
 end
 
 def remove_paths(paths)
-  return unless is_valid_file_path?(paths.first)
-  
-  paths.each { |path| gist_notes.delete_folder(path) }
+  return unless valid_file_path?(paths.first)
 
   puts "removed: #{paths}"
+  paths.each do |path|
+    File.directory?(path) ? gist_notes.delete_folder(path) : gist_notes.add_folder(path)
+  end
 end
 
 def add_paths(paths)
-  return unless is_valid_file_path?(paths.first)
-
-  paths.each { |path| gist_notes.add_folder(path) }
+  return unless valid_file_path?(paths.first)
 
   puts "added: #{paths}"
+  paths.each do |path|
+    gist_notes.add_folder(path)
+  end
 end
 
 def modified_paths(paths)
-  return unless is_valid_file_path?(paths.first)
-
-  paths.each { |path| gist_notes.add_folder(path) }
+  return unless valid_file_path?(paths.first)
 
   puts "modified: #{paths}"
+  paths.each do |path|
+    gist_notes.add_folder(path)
+  end
 end
 
 listener = Listen.to(Config::GIST_NOTES_PATH) do |modified, added, removed|
